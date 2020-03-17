93755f04f337fdd908b3ca357eed93b806a4102f
Vlad Pavlovski <vladislav.pavlovski@ad-in-one.com>
2020-03-17 20:42:16 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["eslint:recommended", "standard", "prettier"],
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ["import", "node", "promise", "standard", "prettier"],
+  rules: {
+    "func-names": "off"
+  }
+};
